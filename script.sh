bf5ff46a46bb5cf211cdf82f204c74d4d9ee4a41
Apig Sharbo <apigsharbo@gmail.com>
2022-02-02 19:09:43 +0400

    add emoji support toggle

--- script.sh
+++ script.sh
@@ -11,6 +11,8 @@
 #   is being inverted using !.  -o errexit
 set -e
 
+EMOJI_SUPPORT='yes'
+
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 sudo apt-get install -y \
@@ -25,7 +27,6 @@ sudo apt-get install -y \
   curl \
   x11-xserver-utils \
   fonts-noto-mono \
-  fonts-noto-color-emoji \
   fonts-symbola \
   linux-headers-$( uname -r ) \
   build-essential \
@@ -52,88 +53,98 @@ pulseaudio --start
 # Unmute audio and set volume to 100
 pulsemixer --unmute --set-volume 100
 
-## Only of you plan to not install libxft-bgra
-# sudo apt-get install -y libxft-dev
+
+if [[ $EMOJI_SUPPORT != 'yes' ]]; then
+
+    ## Only of you plan to not install libxft-bgra
+    sudo apt-get install -y libxft-dev
+fi
 
 ## Install node from "https://github.com/nodesource/distributions/blob/master/README.md"
   # Using Debian, as root
 # sudo bash -c 'curl -fsSL https://deb.nodesource.com/setup_14.x | bash -'
 # sudo apt-get install -y nodejs
 
+if [[ $EMOJI_SUPPORT == 'yes' ]]; then
 
-## Install JoyPixels font
-pkgver='6.6.0'
-fontURL="https://cdn.joypixels.com/arch-linux/font/${pkgver}/joypixels-android.ttf"
-fontDir='/usr/share/fonts/joypixels'
-font="joypixels.ttf"
+    sudo apt install -y fonts-noto-color-emoji
 
-sudo mkdir -p "$fontDir" || { printf '%s\n' "Failed to create dir: $fontDir" 1>&2 && exit 1; }
-sudo curl -LSs -o "$fontDir/$font" "$fontURL"
-[[ -f $fontDir/$font ]] || { printf '%s\n' 'Could not find JoyPixels font.' 1>&2 && exit 1; }
+    ## Install JoyPixels font
+    pkgver='6.6.0'
+    fontURL="https://cdn.joypixels.com/arch-linux/font/${pkgver}/joypixels-android.ttf"
+    fontDir='/usr/share/fonts/joypixels'
+    font="joypixels.ttf"
 
-# Set font priority
-mkdir -p $HOME/.config/fontconfig/conf.d
-cat <<'EOF' > $HOME/.config/fontconfig/conf.d/50-family-defaults.conf
-<?xml version='1.0'?>
-<!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
-<fontconfig>
+    sudo mkdir -p "$fontDir" || { printf '%s\n' "Failed to create dir: $fontDir" 1>&2 && exit 1; }
+    sudo curl -LSs -o "$fontDir/$font" "$fontURL"
+    [[ -f $fontDir/$font ]] || { printf '%s\n' 'Could not find JoyPixels font.' 1>&2 && exit 1; }
 
-   <!-- Override "Noto Color Emoji" when "emoji" family is called -->
-    <match>
-        <test name="family"><string>emoji</string></test>
-        <edit name="family" mode="assign" binding="strong">
-            <string>JoyPixels</string>
-        </edit>
-    </match>
+    # Set font priority.
+    mkdir -p $HOME/.config/fontconfig/conf.d
+    ## ALWAYS MAKE SURE TABS ARE USED FOR INDENTATION
+    cat <<-'EOF' > $HOME/.config/fontconfig/conf.d/50-family-defaults.conf
+    <?xml version='1.0'?>
+    <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+    <fontconfig>
 
-</fontconfig>
-EOF
+       <!-- Override "Noto Color Emoji" when "emoji" family is called -->
+        <match>
+            <test name="family"><string>emoji</string></test>
+            <edit name="family" mode="assign" binding="strong">
+                <string>JoyPixels</string>
+            </edit>
+        </match>
 
-# Apply font priority to all users
-sudo ln -s $HOME/.config/fontconfig/conf.d/50-family-defaults.conf /etc/fonts/conf.d
+    </fontconfig>
+	EOF
+
+    # Apply font priority to all users
+    sudo ln -s $HOME/.config/fontconfig/conf.d/50-family-defaults.conf /etc/fonts/conf.d
 
 
-## libxft-bgra
-## Git clone sometimes take way too long to respond.
-# git clone https://gitlab.freedesktop.org/xorg/lib/libxft.git
-libxftVersion=libxft-master
-libxftArchive=$libxftVersion.tar.gz
-libxftTempFile=/tmp/$libxftArchive
-libxftDir=/opt/$libxftVersion
-libxftRepo="https://gitlab.freedesktop.org/xorg/lib/libxft/-/archive/master/$libxftArchive"
-libxftPatch='1.patch'
-libxftPatchURL="https://gitlab.freedesktop.org/xorg/lib/libxft/merge_requests/$libxftPatch"
+    ## libxft-bgra
+    ## Git clone sometimes take way too long to respond.
+    # git clone https://gitlab.freedesktop.org/xorg/lib/libxft.git
+    libxftVersion=libxft-master
+    libxftArchive=$libxftVersion.tar.gz
+    libxftTempFile=/tmp/$libxftArchive
+    libxftDir=/opt/$libxftVersion
+    libxftRepo="https://gitlab.freedesktop.org/xorg/lib/libxft/-/archive/master/$libxftArchive"
+    libxftPatch='1.patch'
+    libxftPatchURL="https://gitlab.freedesktop.org/xorg/lib/libxft/merge_requests/$libxftPatch"
 
-if [[ ! -d /opt ]]; then
-    sudo mkdir /opt || { printf '%s\n' 'Failed to create dir: /opt' 1>&2 && exit 1; }
+    if [[ ! -d /opt ]]; then
+        sudo mkdir /opt || { printf '%s\n' 'Failed to create dir: /opt' 1>&2 && exit 1; }
+    fi
+
+    sudo curl -LSs -o $libxftTempFile $libxftRepo
+    [[ -f $libxftTempFile ]] || { printf '%s\n' "Could not find file: $libxftTempFile" 1>&2 && exit 1; }
+
+    sudo tar -xzvf $libxftTempFile -C ${libxftDir%/*}
+    [[ -d $libxftDir ]] || { printf '%s\n' "Could not find dir: $libxftDir" 1>&2 && exit 1; }
+
+    sudo curl -LSs -o $libxftDir/$libxftPatch $libxftPatchURL
+    [[ -f $libxftDir/$libxftPatch ]] || { printf '%s\n' "Could not find file: $libxftDir/$libxftPatch" 1>&2 && exit 1; }
+
+    sudo patch -d $libxftDir -p1 < $libxftDir/$libxftPatch
+    sudo sh -c "cd $libxftDir && sh autogen.sh"
+    sudo make -C $libxftDir
+    sudo make -C $libxftDir clean install
+
+
+    ####
+    ## IMPORTANT
+    ## For whatever reason libxft-bgra will still not work, unless nodejs package is installed.
+    ## Even weirder is that after installing nodejs, removing nodejs again will not stop libxft-bgra from working.
+    ## Installing at the start doesn't fix the issue. The following fixed it:
+    ## sudo apt purge --auto-remove nodejs
+    ## The act of interacting with the package AFTER libxft is installed, seems to make libxft work properly.
+    ## I will install it now, and remove it after libxft installation.
+    ####
+    sudo apt-get install -y nodejs
+    # sudo apt-get purge --auto-remove -y nodejs
+
 fi
-
-sudo curl -LSs -o $libxftTempFile $libxftRepo
-[[ -f $libxftTempFile ]] || { printf '%s\n' "Could not find file: $libxftTempFile" 1>&2 && exit 1; }
-
-sudo tar -xzvf $libxftTempFile -C ${libxftDir%/*}
-[[ -d $libxftDir ]] || { printf '%s\n' "Could not find dir: $libxftDir" 1>&2 && exit 1; }
-
-sudo curl -LSs -o $libxftDir/$libxftPatch $libxftPatchURL
-[[ -f $libxftDir/$libxftPatch ]] || { printf '%s\n' "Could not find file: $libxftDir/$libxftPatch" 1>&2 && exit 1; }
-
-sudo patch -d $libxftDir -p1 < $libxftDir/$libxftPatch
-sudo sh -c "cd $libxftDir && sh autogen.sh"
-sudo make -C $libxftDir
-sudo make -C $libxftDir clean install
-
-
-####
-## IMPORTANT
-## For whatever reason libxft-bgra will still not work, unless nodejs package is installed.
-## Even weirder is that after installing nodejs, removing nodejs again will not stop libxft-bgra from working.
-## Installing at the start doesn't fix the issue. The following fixed it:
-## sudo apt purge --auto-remove nodejs
-## The act of interacting with the package AFTER libxft is installed, seems to make libxft work properly.
-## I will install it now, and remove it after libxft installation.
-####
-sudo apt-get install -y nodejs
-# sudo apt-get purge --auto-remove -y nodejs
 
 
 ## Suckless apps:
